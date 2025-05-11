27b4a7c92fc7a3b2bb8db9e5a3cc9fa6680754f2
yukti-says <150407207+Yukti2004@users.noreply.github.com>
2025-05-11 16:38:38 +0530

    initial commits

---
+++ script.js
@@ -0,0 +1,42 @@
+var crsr = document.querySelector("#cursor");
+document.addEventListener("mousemove" , function(dets){
+    crsr.style.left = dets.x + "px"
+    crsr.style.top = dets.y + "px"
+})
+
+var crsrblr = document.querySelector("#cursor-blur");
+document.addEventListener("mousemove" , function(dets){
+    crsrblr.style.left = dets.x - 250 + "px"
+    crsrblr.style.top = dets.y -250 + "px"
+})
+
+
+
+gsap.to("#nav" , {
+
+    backgroundColor : "#000",
+    height : "70px",
+    duration :0.5,
+    scrollTrigger:{
+        trigger:"#nav",
+        scroller:"body",
+        markers:true,
+        start:"top -10%",
+        end : "top -10%",
+        scrub:1
+    }
+   
+})
+
+gsap.to("#main",{
+    backgroundColor: "#000",
+    scrollTrigger:{
+        trigger:"#main",
+        scroller:"body",
+        markers:true,
+        start:"top -25%",
+        end:"top -70%",
+        scrub:2
+
+    }
+})
