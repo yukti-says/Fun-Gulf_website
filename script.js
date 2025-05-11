64911600395be42146047f9963abdf61adef2997
yukti-says <150407207+Yukti2004@users.noreply.github.com>
2025-05-11 19:49:00 +0530

    Updated with some more code

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var crsr = document.querySelector("#cursor");
 document.addEventListener("mousemove" , function(dets){
-    crsr.style.left = dets.x + "px"
+    crsr.style.left = dets.x + 30 + "px"
     crsr.style.top = dets.y + "px"
 })
 
@@ -20,7 +20,7 @@ gsap.to("#nav" , {
     scrollTrigger:{
         trigger:"#nav",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start:"top -10%",
         end : "top -10%",
         scrub:1
@@ -33,10 +33,63 @@ gsap.to("#main",{
     scrollTrigger:{
         trigger:"#main",
         scroller:"body",
-        markers:true,
+        // markers:true,
         start:"top -25%",
         end:"top -70%",
         scrub:2
 
+    }
+})
+
+// select multiple elements on the page 
+ var h4all =  document.querySelectorAll("#nav h4");
+//  these h4 are came in form of node list-treat it as array
+h4all.forEach(function (elem){
+    // console.log(ele)
+    elem.addEventListener("mouseenter" , function(){
+     crsr.style.scale = 2
+     crsr.style.border = "0.5px solid #fff"
+     crsr.style.backgroundColor = "transparent"
+    })
+
+
+    elem.addEventListener("mouseleave" , function(){
+        crsr.style.scale = 1
+        crsr.style.border = "0px solid #95C11E"
+        crsr.style.backgroundColor = "#95C11E"
+       })
+
+})
+
+
+gsap.from("#about-us img , #about-us-in",{
+    y:50,
+    opacity:0,
+    duration:1,
+    // stagger:0.4,
+    scrollTrigger:{
+        trigger: "#about-us",
+        scroller:"body",
+        // marksers:true,
+        start:"top 70%",
+        end: "top 65%",
+        scrub:2
+    }
+})
+
+
+gsap.from(".card",{
+    scale:0.8,
+    opacity:0,
+    duration:1,
+   
+    // stagger:0.4,
+    scrollTrigger:{
+        trigger: ".card",
+        scroller:"body",
+        // marksers:true,
+        start:"top 70%",
+        end: "top 65%",
+        scrub:2
     }
 })
